704dfd4c868dbb6b6f26c44b4abaefc9e113a450
ShingoFukuyama <houseshou@gmail.com>
2014-01-21 22:23:29 +0900

    Correct config explanation

--- helm-css-scss.el
+++ helm-css-scss.el
@@ -24,36 +24,28 @@
 ;; Example config
 
 ;; ----------------------------------------------------
-;; (require 'helm-config)
+;; ;; helm from https://github.com/emacs-helm/helm
+;; (require 'helm)
 
 ;; (add-to-list 'load-path "~/.emacs.d/elisp/helm-css-scss")
 ;; (require 'helm-css-scss)
 
 ;; ;; Allow comment inserting depth at each end of a brace
 ;; (setq helm-css-scss-insert-close-comment-depth 2)
-
-;; ;; If this value is t, split window inside the current window
+;; ;; If this value is t, split window appears inside the current window
 ;; (setq helm-css-scss-split-with-multiple-windows nil)
-
 ;; ;; Split direction. 'split-window-vertically or 'split-window-horizontally
 ;; (setq helm-css-scss-split-direction 'split-window-vertically)
 
 ;; ;; Set local keybind map for css-mode / scss-mode
-;; (dolist ($hook '(css-mode-hook scss-mode-hook less-mode-hook))
+;; (dolist ($hook '(css-mode-hook scss-mode-hook less-css-mode-hook))
 ;;   (add-hook
-;;    $hook
-;;    (lambda ()
-;;      (local-set-key (kbd "s-i") ;; [command + i]
-;;                     'helm-css-scss)
-;;      (local-set-key (kbd "s-b") ;; [command + b]
-;;                     'helm-css-scss-back-to-last-point)
-;;      (local-set-key (kbd "s-c") ;; [command + c]
-;;                     'helm-css-scss-insert-close-comment)
-;;      (local-set-key (kbd "s-n") ;; [command + n]
-;;                     'helm-css-scss-move-and-echo-next-selector)
-;;      (local-set-key (kbd "s-p") ;; [command + p]
-;;                     'helm-css-scss-move-and-echo-previous-selector)
-;;            )))
+;;    $hook (lambda ()
+;;            (local-set-key (kbd "s-i") 'helm-css-scss)
+;;            (local-set-key (kbd "s-I") 'helm-css-scss-back-to-last-point))))
+
+;; (define-key isearch-mode-map (kbd "s-i") 'helm-css-scss-from-isearch)
+;; (define-key helm-css-scss-map (kbd "s-i") 'helm-css-scss-multi-from-helm-css-scss)
 ;; ----------------------------------------------------
 
 ;; This program has two main functions
